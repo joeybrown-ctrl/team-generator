5c74716d2100284e8cc95c4f9c4f875690265be5
Joey Brown <gjoey.brown@gmail.com>
2020-10-16 09:55:15 -0700

    made .gitignore file

--- Develop/app.js
+++ Develop/app.js
@@ -20,6 +20,8 @@ const render = require("./lib/htmlRenderer");
 // }
 // askInitialQuestion().then(meow);
 
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
